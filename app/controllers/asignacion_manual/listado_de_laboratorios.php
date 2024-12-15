55fa53ae21059b149b46ad776db5b3a84dbe9b69
Mario Bautista <mariob784@gmail.com>
2024-12-15 03:19:20 -0600

    labs

--- app/controllers/asignacion_manual/listado_de_laboratorios.php
+++ app/controllers/asignacion_manual/listado_de_laboratorios.php
@@ -7,44 +7,45 @@ if (empty($group_id)) {
     exit;
 }
 
-$sql_subject_ids = "SELECT subject_id FROM group_subjects WHERE group_id = :group_id AND estado = '1'"; 
-$stmt = $pdo->prepare($sql_subject_ids);
-$stmt->bindParam(':group_id', $group_id, PDO::PARAM_INT);
-$stmt->execute();
+try {
+    $sql_group_area = "SELECT area FROM `groups` WHERE group_id = :group_id AND estado = '1'";
+    $stmt = $pdo->prepare($sql_group_area);
+    $stmt->bindParam(':group_id', $group_id, PDO::PARAM_INT);
+    $stmt->execute();
 
-$subjects = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $group = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if (empty($subjects)) {
-    echo json_encode(['status' => 'error', 'message' => 'No se encontró ninguna materia asociada al grupo o el grupo está inactivo.']);
+    if (empty($group)) {
+        echo json_encode(['status' => 'error', 'message' => 'No se encontró el grupo o el grupo está inactivo.']);
+        exit;
+    }
+
+    $group_area = $group['area'];
+
+    $sql_labs = "SELECT DISTINCT
+                    l.lab_id, 
+                    l.lab_name, 
+                    l.fyh_creacion, 
+                    l.description
+                 FROM 
+                    labs l
+                 WHERE 
+                    FIND_IN_SET(:group_area, l.area) > 0";
+
+    $stmt = $pdo->prepare($sql_labs);
+    $stmt->bindParam(':group_area', $group_area, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $labs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    error_log("Labs found: " . count($labs));
+
+    if (empty($labs)) {
+        $labs = [];
+    }
+
+
+} catch (PDOException $e) {
     exit;
 }
-
-error_log("subject_ids found: " . count($subjects));
-
-$subject_ids = array_map(function($subject) { return $subject['subject_id']; }, $subjects);
-$subject_ids_placeholder = implode(',', array_fill(0, count($subject_ids), '?'));
-
-$sql_labs = "SELECT DISTINCT
-                l.lab_id, 
-                l.lab_name, 
-                l.fyh_creacion, 
-                l.description
-            FROM 
-                labs l
-            INNER JOIN 
-                subject_labs sl ON l.lab_id = sl.lab_id
-            WHERE 
-                sl.subject_id IN ($subject_ids_placeholder)";
-
-$stmt = $pdo->prepare($sql_labs);
-$stmt->execute($subject_ids);
-
-$labs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-error_log("Labs found: " . count($labs));
-
-if (empty($labs)) {
-    $labs = [];
-}
-
 ?>
